c7c1d4230f635fb2d80ad68c506da7e7d7149f57
= <hiagobrenha@protonmail.com>
2025-06-05 11:43:54 -0300

    chore: add styles to  categories list datagrid

--- src/features/categories/CategoryList.tsx
+++ src/features/categories/CategoryList.tsx
@@ -1,8 +1,8 @@
-import { Box, Button } from "@mui/material"
+import { Box, Button, IconButton, Typography } from "@mui/material"
 import { useAppSelector } from "../../app/hooks"
 import { selectCategories } from "./categorySlice"
 import { Link } from "react-router"
-import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid"
+import { DataGrid, GridColDef, GridDeleteIcon, GridRenderCellParams, GridRowsProp } from "@mui/x-data-grid"
 
 export default function CategoryList() {
   const categories = useAppSelector(selectCategories)
@@ -10,14 +10,51 @@ export default function CategoryList() {
   const rows: GridRowsProp = categories.map((category) => ({
     id: category.id,
     name: category.name,
-    description: category.description
+    description: category.description,
+    isActive: category.is_active,
+    createdAt: new Date(category.created_at).toLocaleDateString('pt-BR')
   }))
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 150 },
-    { field: 'name', headerName: 'Name', width: 150 },
-    { field: 'description', headerName: 'Description', width: 150 },
+    { field: 'name', headerName: 'Name', flex: 1 },
+    { field: 'description', headerName: 'Description', flex: 1 },
+    {
+      field: 'isActive',
+      headerName: 'Active?',
+      flex: 1,
+      type: "boolean",
+      renderCell: renderIsActiveCell
+    },
+    {
+      field: 'createdAt',
+      headerName: 'Created at',
+      flex: 1
+    },
+    {
+      field: 'id',
+      headerName: 'Actions',
+      flex: 1,
+      renderCell: renderActionCell
+    },
   ]
+
+  function renderIsActiveCell(rowData: GridRenderCellParams) {
+    return <Typography color={rowData.value ? "primary" : "secondary"}>
+      {rowData.value ? "Active" : "Inactive"}
+    </Typography>
+  }
+
+  function renderActionCell(rowData: GridRenderCellParams) {
+    return (
+      <IconButton
+        color="secondary"
+        onClick={() => console.log("Delete")}
+        aria-label="delete"
+      >
+        <GridDeleteIcon />
+      </IconButton>
+    )
+  }
 
   return (
     <Box maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
@@ -33,7 +70,14 @@ export default function CategoryList() {
         </Button>
       </Box>
       <div style={{ height: 300, width: "100%" }}>
-        <DataGrid rows={rows} columns={columns} />
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          showToolbar={true}
+          disableColumnSelector={true}
+          disableColumnFilter={true}
+          disableRowSelectionOnClick={true}
+        />
       </div>
     </Box>
   )
